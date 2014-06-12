0132cdf43e7574c95a571a9b3805b2f9f360c35d
MDM <w21109420044@gmail.com>
2014-06-11 21:04:47 -0700

    Using socket lib to recv data

--- WebsocketServerProtocol/examples/WebsocketProt/WebsocketProt.ino
+++ WebsocketServerProtocol/examples/WebsocketProt/WebsocketProt.ino
@@ -2,12 +2,12 @@
  Advanced Chat Server Protocol
 
  This example demostrate a simple echo server.
- It demostrate how the library <WebSocketProtocol.h> works 
+ It demostrate how the library <WebSocketProtocol.h> works
  and how to handle the state changes.
- 
- dependent library:WIZNET <Ethernet.h>
- 
 
+ dependent library:WIZNET <Ethernet.h>
+
+ 
 
  created 11 June 2014
  by MDM Tseng
@@ -17,24 +17,22 @@
 #include <Ethernet.h>
 #include <WebSocketProtocol.h>
 #include "utility/w5100.h"
+#include "utility/socket.h"
 byte mac[] = {
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
 };
 IPAddress ip(10, 0, 0, 52);
 IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 0, 0);
-EthernetServer server(5213);
 
+EthernetServer server(5213);
 WebSocketProtocol WSP[4];
 
 char buff[600];
 char *buffiter;
 void setup() {
-  // initialize the ethernet device
   Ethernet.begin(mac, ip, gateway, subnet);
-  // start listening for clients
   server.begin();
-  // Open serial communications and wait for port to open:
   Serial.begin(57600);
 
 
@@ -47,26 +45,21 @@ void loop() {
 
   if (client) {
 
-  clearUnavalibleClient();
+    clearUnavalibleClient();
     buffiter = buff;
-    byte KL = 0;
-    buffiter=buff;
-    KL=client.available();
-    for(;KL;KL--) {
-      *(buffiter++)=client.read();
-    }
-    WebSocketProtocol* WSPptr  =findFromProt(client);
-      Serial.println(WSPptr-WSP);
-    if(WSPptr==null)
+    unsigned int  KL = 0;
+
+    unsigned int PkgL =  client.available();
+    KL = PkgL;
+    recv(client._sock,(uint8_t*)buffiter,PkgL);
+    WebSocketProtocol* WSPptr  = findFromProt(client);
+    if (WSPptr == null)
     {
       client.stop();
       return;
     }
-    client=WSPptr->getClientOBJ();
-    
+    client = WSPptr->getClientOBJ();
     char *recvData = WSPptr->processRecvPkg(buff, KL);
-    //WSP.printState();
-    //Serial.println();
     if (WSPptr->getState() == WS_HANDSHAKE)
     {
       client.print(buff);
@@ -78,17 +71,15 @@ void loop() {
       WSPptr->rmClientOBJ();
       return;
     }
-    if(WSPptr->getRecvOPState() == WSOP_UNKNOWN)
+    if (WSPptr->getRecvOPState() == WSOP_UNKNOWN)
     {
       client.print(WSPptr->codeSendPkg_endConnection(buff));
-      
+
       client.stop();
       WSPptr->rmClientOBJ();
       return;
     }
-    
-    //WSP.printRecvOPState();
-    //Serial.println();
+
     recvData -= 2;
     recvData[0] = 0x81;
     client.print(recvData); return;
@@ -96,10 +87,10 @@ void loop() {
 }
 void clearUnavalibleClient()
 {
-  for(byte i=0;i<4;i++)
+  for (byte i = 0; i < 4; i++)
   {
-    EthernetClient Rc=WSP[i].getClientOBJ();
-    if(Rc&&Rc.status()==SnSR::CLOSED)
+    EthernetClient Rc = WSP[i].getClientOBJ();
+    if (Rc && Rc.status() == SnSR::CLOSED)
     {
       Serial.print("clear unused sock::");
       Serial.println(Rc._sock);
@@ -112,30 +103,30 @@ void clearUnavalibleClient()
 
 WebSocketProtocol* findFromProt(EthernetClient client)
 {
-  
-  for(byte i=0;i<4;i++)
+
+  for (byte i = 0; i < 4; i++)
   {
-    EthernetClient Rc=WSP[i].getClientOBJ();
-    if(Rc==client)
+    EthernetClient Rc = WSP[i].getClientOBJ();
+    if (Rc == client)
     {
-      
-      Serial.print(i);Serial.print("::");Serial.print(Rc);Serial.print("==");Serial.println(client);
-      return WSP+i;
+
+      Serial.print(i); Serial.print("::"); Serial.print(Rc); Serial.print("=="); Serial.println(client);
+      return WSP + i;
     }
   }
-  
-  Serial.println("NO exist client, find available");
-  for(byte i=0;i<4;i++)
+
+  Serial.println("NO exist, find available");
+  for (byte i = 0; i < 4; i++)
   {
-    if(!WSP[i].getClientOBJ())
+    if (!WSP[i].getClientOBJ())
     {
-      
+
       Serial.print(i);
       Serial.print("  ::::  ");
       Serial.println(WSP[i].getClientOBJ());
       WSP[i].setClientOBJ(client);
-      return WSP+i;
-    } 
+      return WSP + i;
+    }
   }
   return null;
 }
